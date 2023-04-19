ba02d187716a8a391d6daec3ef11fe0294431ae4
Neha <nehatualea@gmail.com>
2023-04-20 02:12:39 +0530

    Solution 2

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Logo from './ReactLogo.js';
+import ReactBenifits from './ReactBenifts';
+import Button from './Button';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-       <h1>Benifits of using react ...</h1>
-       <ol>
-        <li>Component based architecture</li>
-        <li>Virtual DOM for efficient updates</li>
-        <li>Rich ecosystem and community</li>
-        <li>Cross platform development</li>
-        <li>strong community support</li>
-       </ol>
-
-       <button>Get Stareted...</button>
+        <Logo/>
+        <ReactBenifits/>
+        <Button/>
+       
+  
       </header>
     </div>
   );
---
+++ src/ReactBenifts.js
@@ -0,0 +1,19 @@
+import { Fragment } from "react"
+
+
+export default function ReactBenifits(){
+    return (
+        <Fragment>
+        <h1>Benifits of using react ...</h1>
+        <ol>
+         <li>Component based architecture</li>
+         <li>Virtual DOM for efficient updates</li>
+         <li>Rich ecosystem and community</li>
+         <li>Cross platform development</li>
+         <li>strong community support</li>
+        </ol>
+        </Fragment>
+
+        
+    );
+}
