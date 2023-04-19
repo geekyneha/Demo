159576bafa8a7c63ba7eed52df8386e1d0873308
Neha <nehatualea@gmail.com>
2023-04-20 01:26:18 +0530

    solution 1

--- src/App.js
+++ src/App.js
@@ -6,17 +6,16 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1>Benifits of using react ...</h1>
+       <ol>
+        <li>Component based architecture</li>
+        <li>Virtual DOM for efficient updates</li>
+        <li>Rich ecosystem and community</li>
+        <li>Cross platform development</li>
+        <li>strong community support</li>
+       </ol>
+
+       <button>Get Stareted...</button>
       </header>
     </div>
   );
